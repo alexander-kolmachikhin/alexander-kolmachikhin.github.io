6b4121031a6de07af7ddcee632e9f365f029f912
Alexander Kolmachikhin <40714382+alexander-kolmachikhin@users.noreply.github.com>
2019-12-31 18:12:47 +0700

    Add files via upload

---
+++ scripts/menu.js
@@ -0,0 +1,9 @@
+var menu = {
+	open: () => {
+	  document.getElementById("menu").style.marginLeft = "0px";
+	},
+
+	close: () => {
+	  document.getElementById("menu").style.marginLeft = "-249px";
+	}
+}
