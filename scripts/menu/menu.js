d46d696f3bd72d60a83b96d7044558bd414e481e
Alexander Kolmachikhin <40714382+alexander-kolmachikhin@users.noreply.github.com>
2019-12-31 18:09:04 +0700

    Delete menu.js

--- scripts/menu/menu.js
+++
@@ -1,9 +0,0 @@
-var menu = {
-	open: () => {
-	  document.getElementById("menu").style.marginLeft = "0px";
-	},
-
-	close: () => {
-	  document.getElementById("menu").style.marginLeft = "-249px";
-	}
-}
